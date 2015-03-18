04e8cf1abb20397d5bb08faf1970fbeb3902e03b
bocasfx <rpalacios75@gmail.com>
2015-03-18 19:38:44 -0400

    Allow fro multiple icons

--- js/sixtycycles.js
+++ js/sixtycycles.js
@@ -16,7 +16,8 @@ var sc = {
 
       var name = sc.projects[i].name;
       var desc = sc.projects[i].description;
-      var icon = sc.projects[i].icon;
+      var icons = sc.projects[i].icons;
+
 
       var cellAnchor = $("<a>", {
         "href": "#",
@@ -33,13 +34,13 @@ var sc = {
       innerCell.text(name);
       
       var backCell = $("<div>", {"class": "back-cell right"});
-      var backCheck = $("<span>", {"class": "back-check fa " + icon});
-      var backTitle = $("<span>");
+      
+      var backTitle = $("<div>");
       backTitle.text(name);
       var backDescription = $("<div>", {"class": "back-description"});
       backDescription.text(desc);
-      backCell.append(backCheck);
       backCell.append(backTitle);
+      icons.forEach(sc.appendIcon, backCell);
       backCell.append(backDescription);
 
       cellAnchor.append(innerCell);
@@ -54,7 +55,13 @@ var sc = {
     }
   },
 
+  appendIcon: function(item, index) {
+    var cellIcon = $("<span>", {"class": "cell-icon fa " + item});
+    this.append(cellIcon);
+  },
+
   populateModal: function(event) {
+    $("#sc-modal").empty();
     $("#sc-modal").load("./modals/" + event.data.id + ".html");
   }
 };
