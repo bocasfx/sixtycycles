e0f5e95b87d67e258e9d0c3d091f94f187f3dc9d
bocasfx <rpalacios75@gmail.com>
2015-03-19 15:12:54 -0400

    Using SASS, Bower and Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,49 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var bower = require('gulp-bower');
+var livereload = require('gulp-livereload');
+var del = require('del');
+var $ = require('gulp-load-plugins')({lazy: true});
+
+var config = {
+  sassPath: './sass',
+  bowerDir: './bower_components',
+  sass: ['./sass/**/*.scss', './sass/**/*.sass']
+};
+
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest(config.bowerDir));
+});
+
+gulp.task('icons', function() {
+  return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+    .pipe(gulp.dest('./www/fonts'));
+});
+
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch(config.sass, ['sass']);
+});
+
+gulp.task('sass', function() {
+  return gulp
+    .src(config.sassPath + '/sixtycycles.sass')
+    .pipe(sass({
+      sourceComments: 'normal',
+      style: 'compressed',
+      includePaths: [
+        './sass',
+        config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+        config.bowerDir + '/fontawesome/scss',
+      ]
+    }))
+    .pipe($.minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe($.rename({extname: '.min.css'}))
+    .pipe(gulp.dest('./www/css'))
+    .pipe(livereload());
+});
