1d5af33faf9a2c0067c04eb0056eaeb0b5cf24f5
bocasfx <rpalacios75@gmail.com>
2015-03-20 20:32:43 -0400

    Aboot section. More projects

--- www/js/sixtycycles.js
+++ www/js/sixtycycles.js
@@ -1,81 +1,106 @@
-var sc = {
+(function() {
 
-  createCells : function () {
-    var idx = 0;
+  var sc = {
 
-    for (var i = 0; i < sc.projects.length; i++) {
+    createCells : function () {
+      var idx = 0;
 
-      var bgidx = (idx % sc.colors.length);
-      var fgidx = ((idx + 2) % sc.colors.length);
+      for (var i = 0; i < sc.projects.length; i++) {
 
-      bgcolor = sc.colors[bgidx];
-      fgcolor = sc.colors[fgidx];
+        var bgidx = (idx % sc.colors.length);
+        var fgidx = ((idx + 2) % sc.colors.length);
 
-
-      var name = sc.projects[i].name;
-      var desc = sc.projects[i].description;
-      var icons = sc.projects[i].icons;
+        bgcolor = sc.colors[bgidx];
+        fgcolor = sc.colors[fgidx];
 
 
-      var cellAnchor = $("<a>", {
-        "href": "#",
-        "data-toggle": "modal",
-        "data-target": "#sc-modal"
-      });
-      cellAnchor.click({"id": sc.projects[i].id}, sc.populateModal);
-      
-      var innerCell = $("<div>", {"class": "inner-cell cell-link"});
-      innerCell.css({
-        "background-color": bgcolor,
-        "color": fgcolor
-      });
-      innerCell.text(name);
-      
-      var backCell = $("<div>", {"class": "back-cell right"});
-      
-      var backTitle = $("<div>");
-      backTitle.text(name);
-      var backDescription = $("<div>", {"class": "back-description"});
-      backDescription.text(desc);
-      backCell.append(backTitle);
+        var name = sc.projects[i].name;
+        var desc = sc.projects[i].description;
+        var icons = sc.projects[i].icons;
 
-      var iconPrefs = {
-        backCell: backCell
-      };
 
-      icons.forEach(sc.appendIcon, iconPrefs);
-      backCell.append(backDescription);
+        var cellAnchor = $("<a>", {
+          "href": "#",
+          "data-toggle": "modal",
+          "data-target": "#sc-modal"
+        });
+        cellAnchor.click({"id": sc.projects[i].id}, sc.populateModal);
+        cellAnchor.css({
+          "outline": 0,
+          "text-decoration": "none"
+        });
+        
+        var innerCell = $("<div>", {"class": "inner-cell cell-link"});
+        innerCell.css({
+          "background-color": bgcolor,
+          "color": fgcolor
+        });
+        innerCell.text(name);
+        
+        var backCell = $("<div>", {"class": "back-cell right"});
+        
+        var backTitle = $("<div>");
+        backTitle.text(name);
+        var backDescription = $("<div>", {"class": "back-description"});
+        backDescription.text(desc);
+        backCell.append(backTitle);
 
-      cellAnchor.append(innerCell);
+        var iconPrefs = {
+          backCell: backCell
+        };
 
-      var cell = $("<div>", {"class": "col-md-4 cell right"});
-      cell.append(cellAnchor);
-      cell.append(backCell);
+        icons.forEach(sc.appendIcon, iconPrefs);
+        backCell.append(backDescription);
 
-      $( "#cell-container" ).append( cell );
+        cellAnchor.append(innerCell);
 
-      idx += 1;
+        var cell = $("<div>", {"class": "col-md-4 cell right"});
+        cell.append(cellAnchor);
+        cell.append(backCell);
+
+        $( "#cell-container" ).append( cell );
+
+        idx += 1;
+      }
+    },
+
+    appendIcon: function(item, index) {
+      var cellIcon = $("<span>", {"class": "cell-icon fa " + item});
+      this.backCell.append(cellIcon);
+    },
+
+    populateModal: function(event) {
+      $("#sc-modal").empty();
+      $("#sc-modal").load("./modals/" + event.data.id + ".html");
+    },
+
+    generaterAboutSection: function() {
+      $(".aboot").click({"id": "about"}, sc.populateModal);
+    },
+
+    unmentionableBrowserDetected: function() {
+      var ua = window.navigator.userAgent;
+      var msie = ua.indexOf("MSIE ");
+
+      if (msie > 0) {
+        return true;
+      }
+      return false;
     }
-  },
+  };
 
-  appendIcon: function(item, index) {
-    var cellIcon = $("<span>", {"class": "cell-icon fa " + item});
-    this.backCell.append(cellIcon);
-  },
-
-  populateModal: function(event) {
-    $("#sc-modal").empty();
-    $("#sc-modal").load("./modals/" + event.data.id + ".html");
-  }
-};
-
-$( document ).ready(function() {
-  $.getJSON("config.json", function(data) {
-    sc.projects = data.projects;
-    sc.colors = data.colors;
-    sc.createCells();
+  $( document ).ready(function() {
+    if (sc.unmentionableBrowserDetected()) {
+      window.alert("We're sorry. Sixtycycles does not support the use of subpar browsers.");
+      return;
+    }
+    $.getJSON("config.json", function(data) {
+      sc.projects = data.projects;
+      sc.colors = data.colors;
+      sc.generaterAboutSection();
+      sc.createCells();
+    });
   });
-});
-
+})();
 
 
